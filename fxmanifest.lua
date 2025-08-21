9ac5fcaeec0cd7eccd89cff32f98e0834036aeb5
Modrejsell <136122579+Modrejsell@users.noreply.github.com>
2025-08-21 19:05:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DevNest'
+description 'Low Loading Screen'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/music.mp3',
+    'html/img1.jpg',
+    'html/img2.jpg',
+    'html/img3.jpg'
+}
